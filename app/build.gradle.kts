0732e3290171852c7731194c89996dc0c537b89a
Rober_Car <pollinox@hotmail.com>
2025-12-17 11:14:08 +0100

    configurando el recycler y el adapter de los personajes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
 	implementation("com.google.firebase:firebase-database:20.3.0")
 	implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
 	implementation("com.google.firebase:firebase-database-ktx:20.3.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
 
 
--- app/src/main/java/com/example/rmviewer/adapter/AdaptadorPersonajes.kt
+++ app/src/main/java/com/example/rmviewer/adapter/AdaptadorPersonajes.kt
@@ -3,55 +3,52 @@ package com.example.rmviewer.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
 import com.example.rmviewer.R
 import com.example.rmviewer.databinding.ItemEpisodioBinding
+import com.example.rmviewer.databinding.ItemPersonajeBinding
 import com.example.rmviewer.model.Episodio
+import com.example.rmviewer.model.Personaje
 
-/**
- * Adaptador para mostrar episodios en un RecyclerView.
- * Recibe un callback 'onClick' para manejar clicks en cada item.
- */
-class AdaptadorEpisodios(
-    private val onClick: (Episodio) -> Unit   // Callback que se ejecuta al pulsar un episodio
-) : RecyclerView.Adapter<AdaptadorEpisodios.MyViewHolder>() {
+class AdaptadorPersonajes(
+    private val onClick: (Personaje) -> Unit
+): RecyclerView.Adapter<AdaptadorPersonajes.MyViewHolder>() {
 
     // --------------------------------------------------------
     // BLOQUE 1: LISTA DE DATOS INTERNA
     // --------------------------------------------------------
     // Usamos una lista mutable para poder actualizarla cuando lleguen datos de la API.
-    private val listaEpisodios = mutableListOf<Episodio>()
 
+    private val listaPersonajes = mutableListOf<Personaje>()
 
     // --------------------------------------------------------
     // BLOQUE 2: VIEW HOLDER (gestiona cada tarjeta/ítem del RecyclerView)
     // --------------------------------------------------------
     //class MyViewHolder → es una clase que define cómo se guarda y maneja la vista de un ítem.n
     // es un contenedor de vistas que representa un ítem de la lista.
-    //inner class es una clase declarada dentro de otra clase.La palabra clave inner le da
+    //inner class es una clase declarada dentro de otra clase.inner le da
     // a esa clase interna acceso a las propiedades y métodos de la clase externa.
-    inner class MyViewHolder(private val binding: ItemEpisodioBinding) :
+    //Recibe  una instancia del binding del layout que representa cada ítem del RecyclerView.
+
+    inner class MyViewHolder(private val binding: ItemPersonajeBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        //bind es una función personalizada se encarga de recibir un objeto de datos (como un Episodio,
-        // Post, etc.) Y rellenar las vistas del layout del ítem con esos datos.
-        fun bind(episodio: Episodio) {
+        fun bind(personaje: Personaje) {
 
+            binding.itemNombrePersonaje.text = personaje.name
+            // 1. Prepara a Glide indicándole el "contexto" (la actividad o fragmento).
+            Glide.with(binding.imgPersonaje.context)
 
-            // Muestra los textos del item
-            binding.itemNombreEpisodio.text = episodio.name
-            binding.itemCodigoEpisodio.text = episodio.episode  //fecha
-            binding.itemFechaEpisodio.text = episodio.air_date //codigo
+                //Define QUÉ imagen cargar.
+                .load(personaje.image)
 
-            // --------------------------------------------------------
-            // BLOQUE 2.2: CLICK EN EL ITEM
-            // --------------------------------------------------------
-            binding.root.setOnClickListener {
-                onClick(episodio)
-            }
+                // Define DÓNDE mostrarla.
+                // 'into' toma la imageny la coloca automáticamente en el ImageView.
+                .into(binding.imgPersonaje)
         }
+
     }
-
-
     // --------------------------------------------------------
     // BLOQUE 3: CREACIÓN DEL VIEW HOLDER
     // --------------------------------------------------------
@@ -60,13 +57,13 @@ class AdaptadorEpisodios(
     // para construir esas cajas.
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
         // Inflamos el layout XML del item
-        val binding = ItemEpisodioBinding.inflate(
+        val binding = ItemPersonajeBinding.inflate(
             LayoutInflater.from(parent.context),
             parent,
             false
         )
-
         //Metemos el diseño dentro del contenedor (ViewHolder):
         return MyViewHolder(binding)
     }
@@ -77,14 +74,13 @@ class AdaptadorEpisodios(
     // --------------------------------------------------------
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.bind(listaEpisodios[position])
+        holder.bind((listaPersonajes[position]))
     }
-
 
     // --------------------------------------------------------
     // BLOQUE 5: TAMAÑO TOTAL DE ITEMS
     // --------------------------------------------------------
-    override fun getItemCount(): Int = listaEpisodios.size
+    override fun getItemCount(): Int = listaPersonajes.size
 
 
     // --------------------------------------------------------
@@ -94,9 +90,10 @@ class AdaptadorEpisodios(
      * Sustituye el contenido del adaptador por una nueva lista.
      * Se llama desde el Fragment cuando llega la respuesta de Retrofit.
      */
-    fun setData(newList: List<Episodio>) {
-        listaEpisodios.clear()         // Borra los episodios actuales
-        listaEpisodios.addAll(newList) // Añade los nuevos
+
+    fun setData(newList: List<Personaje>) {
+        listaPersonajes.clear()         // Borra los episodios actuales
+        listaPersonajes.addAll(newList) // Añade los nuevos
         notifyDataSetChanged()         // Actualiza la vista para ver los cambios
     }
 }
